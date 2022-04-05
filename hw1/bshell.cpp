63b5a00afa126a69c8cf84c82f66f3ca97c352b9
bugra from inek <e2380079@newinek>
2022-04-05 18:25:00 +0300

    pipeline pred part

--- hw1/bshell.cpp
+++ hw1/bshell.cpp
@@ -40,7 +40,7 @@ int main(){
     string inp;
     int is_bundle_creation = 0;
     bundle *new_bundle = nullptr;
-    vector<bundle> bundles;
+    vector<bundle> registered_bundles;
     while(true){
         getline(cin,inp);
         inp.push_back(' ');         // ADJUSTING INPUT
@@ -60,7 +60,7 @@ int main(){
                     bundle to_be_executed;
                     bundle_execution *execution = input.command.bundles; //in this case, we use only bundles[0] 
                     bool found = false;
-                    for(bundle b : bundles){//FIND THE BUNDLE
+                    for(bundle b : registered_bundles){//FIND THE BUNDLE
                         if(bundle_name.compare(b.bundle_name) == 0){
                             to_be_executed = b;
                             found = true;
@@ -107,8 +107,76 @@ int main(){
                     }
 
                 }
-                else{
+                else{ //pipelining here
+                    bundle_execution *bundles = input.command.bundles;
+                    for(int i=0;i<input.command.bundle_count-1;i++){
+                        pid_t spid = fork();
+                        if(spid == 0){ //sender process for bundle1
+                            pid_t rep_pid = fork();
+                            int fd_pred[2];
+                            pipe(fd_pred);
+                            //create a pipe here with fd_pred
+                            if(rep_pid > 0){ //sender process, execute bundles[i], writer
+                                close(fd_pred[0]); //close read end
+                                dup2(fd_pred[1],1); //redirect output to fd_pred[1]
+                                //find the bundle
+                                string bundle_name(input.command.bundles[i].name);
+                                bundle to_be_executed; 
+                                bool found = false;
+                                for(bundle b : registered_bundles){//FIND THE BUNDLE
+                                    if(bundle_name.compare(b.bundle_name) == 0){
+                                        to_be_executed = b;
+                                        found = true;
+                                        break;
+                                    }
+                                }
+                                if(found){
+                                    //fork and exec for each
+                                    int nump = to_be_executed.processes.size();
+                                    pid_t pid[nump];
+                                    int child_status;
+                                    for(int j=0;j<nump;j++){// FORK NUMBER OF PROCESSES TIME AND EXECUTE EACH PROCESS IN ONE CHILD
+                                        if((pid[j] = fork()) == 0){ //child process
+                                            //execute i'th process in the child.
+                                            //convert vector<string> process to char**, call execv()
+                                            if(bundles[i].input){// if there is a input redirection
+                                                char* in = bundles[i].input;
+                                                int fd_in = open(in, O_RDONLY);
+                                                dup2(fd_in,0);
+                                            }
+                                            char** process;
+                                            converter(to_be_executed.processes[i],process);
+                                            execv(process[0],process);
+                                            return 1;
+                                        }
+                                    }
+                                    for(int k=0;k<nump;k++) //REAP ALL CHILDS
+                                        wait(&child_status);
+                                    return 1;
+                                }
+                                else{
+                                    cout << "Bundle " << bundle_name << " can not be found" << endl;
+                                }
+                                
+                            }
+                            else{//repeater process
+                                //redirect the input to fd_pred[0]
+                                //create a pipe for each process in bundles[i+1]
+                                pid_t rpid = fork();
+                                if(rpid>0){ //repeater process
+                                    //redirect the output to each pipe
+                                }
+                                else{ //receiver process
+                                    //fork for each process
+                                    //redirect each process' input to corresponding pipe
+                                    //exec process
+                                }
 
+                                
+                            }
+                        }
+
+                    }
                 }
                 
             }
@@ -125,11 +193,11 @@ int main(){
             try{ // check whether it is a special command
                 if(input.command.type == PROCESS_BUNDLE_STOP){ //store created bundle, delete new_bundle and new_bundle = nullptr
                         cout << "Bundle creation is stopped. Registering the bundle " << new_bundle->bundle_name << endl;
-                        bundles.push_back(*new_bundle); //store
+                        registered_bundles.push_back(*new_bundle); //store
                         delete new_bundle; //deallocate
                         new_bundle = nullptr; // make it nullptr again
                         is_bundle_creation = 0;
-                        printBundles(bundles);
+                        printBundles(registered_bundles);
                 }
                 else
                     throw 0;
